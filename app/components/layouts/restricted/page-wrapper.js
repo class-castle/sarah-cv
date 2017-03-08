f2a94c8817041ddfe045040da8a80ee5ea031a6d
Andrew Paterson <andrew@hyraxbio.co.za>
2017-03-08 18:12:54 +0200

    Added references and introduction content.

--- app/components/layouts/restricted/page-wrapper.js
+++ app/components/layouts/restricted/page-wrapper.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	session: Ember.inject.service(),
   	tagName: 'div',
-	classNames: ['wrapper', 'interface'],
+	classNames: ['wrapper', 'interface', 'text-column'],
 	classNameBindings: ['classes', 'session.navCollapsed:nav-collapsed',  'session.navToggled:nav-toggled', 'session.placeholdersSupported:placeholders', 'interfaceTopBar:top-bar-present', 'session.userUiState.freezeNavigation:nav-frozen', 'session.hideContent:hide-content:show-content'],
 });
--- app/routes/introduction.js
+++ app/routes/introduction.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	test: {
+		'name': 'Sarah Schafer',
+		'text': 'Hello World',
+	},
 });
